753a565cb72f92f346be69a9604934f87a7c8175
Administrator <administrator@MacBook-Pro-von-Administrator.local>
2023-04-08 23:30:30 +0200

    Trying to solve issue with Movie DB API

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express'); 
 const app = express();              
-const port = 5001;                  
+const port = 5001;  
+                
+
+
+async function logJSONData() {
+    
+    const response = await fetch("https://api.themoviedb.org/3/movie/76341?api_key=d603f8d2da405d422c0b6dd441888e5c&language=de");
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
+
+
+  
 
 app.get('/', (req, res) => { 
-    console.log("Loggeds")
-});
 
-app.listen(port, () => {            
-    console.log(`Now listening on port ${port}`); 
+
+
+    
 });
