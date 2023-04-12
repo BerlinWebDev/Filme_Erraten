084aecc82aef385a80127c434132187fa219da59
BerlinWebDev <berlinwebentwickler@gmail.com>
2023-04-13 01:24:19 +0200

    Problem gelöst

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import NavBar from "./components/NavBar";
 function App() {
   const [movieData, setMovieData] = useState(null);
 
-  useEffect(async () => {
-    await fetch(
+  useEffect(() => {
+    fetch(
       "https://api.themoviedb.org/3/movie/550?api_key=d603f8d2da405d422c0b6dd441888e5c"
     ).then((actualData) => console.log(actualData));
     return () => {}; // no-op
