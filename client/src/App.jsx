c7e35dbb420f03e580d398c3c2c3ba983d488e04
BerlinWebDev <berlinwebentwickler@gmail.com>
2023-04-13 01:20:18 +0200

    Fehler in Chrome genauer anschauen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [movieData, setMovieData] = useState(null);
+
+  useEffect(async () => {
+    await fetch(
+      "https://api.themoviedb.org/3/movie/550?api_key=d603f8d2da405d422c0b6dd441888e5c"
+    ).then((actualData) => console.log(actualData));
+    return () => {}; // no-op
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <div className="App">Hello</div>
+    </>
+  );
 }
 
 export default App;
