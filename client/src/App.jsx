c1dde4caaa26a3a17b94f11918c7e859eb440389
BerlinWebDev <berlinwebentwickler@gmail.com>
2023-04-15 13:52:41 +0200

    Working API Request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
 import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
+import axios from "axios";
+import { version } from "react";
 
 function App() {
-  const [movieData, setMovieData] = useState(null);
-
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/550?api_key=d603f8d2da405d422c0b6dd441888e5c"
-    ).then((actualData) => console.log(actualData));
-    return () => {}; // no-op
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=d603f8d2da405d422c0b6dd441888e5c&language=de&page=1"
+      )
+      .then((response) => {
+        setMovies(response.data.results);
+        console.log(movies);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
     <>
       <NavBar />
-      <div className="App">Hello</div>
+      {movies.map((movie) => (
+        <p key={movie.id}>{movie?.original_title}</p>
+      ))}
+      <div className="App"></div>
     </>
   );
 }
